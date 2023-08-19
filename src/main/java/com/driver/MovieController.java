e287e137ec55876544740e30faa288fec4d0863c
Rahul Kishne <kishnerahul123@gmail.com>
2023-08-19 20:27:55 +0530

    First Commit

---
+++ src/main/java/com/driver/MovieController.java
@@ -0,0 +1,53 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    @Autowired
+    MovieService movieService;
+
+    @PostMapping("/add-movie")
+    public ResponseEntity<String> addMovie(@RequestBody Movie movie){
+        return ResponseEntity.ok(movieService.addMovie(movie));
+    }
+
+    @PostMapping("/add-director")
+    public ResponseEntity<String> addDirector(@RequestBody Director director){
+        return ResponseEntity.ok(movieService.addDirector(director));
+    }
+
+    @PutMapping("/add-movie-director-pair")
+    public ResponseEntity<String> addMovieDirectorPair(@RequestParam("mname") String movieName,@RequestParam("dname") String directorName){
+        return ResponseEntity.ok(movieService.addMovieDirectorPair(movieName,directorName));
+    }
+    @GetMapping("/get-movies-by-name/{name}")
+    public ResponseEntity<Movie> getMovieByName(@PathVariable("name") String movieName){
+        return ResponseEntity.ok(movieService.getMovieByName(movieName));
+    }
+    @GetMapping("/get-director-by-name/{name}")
+    public ResponseEntity<Director> getDirectorByName(@PathVariable("name") String directorName){
+        return ResponseEntity.ok(movieService.getDirectorByName(directorName));
+    }
+    @GetMapping("/get-movies-by-director-name/{name}")
+    public ResponseEntity<List<String>> getMoviesByDirectorName(@PathVariable("name") String directorName){
+        return ResponseEntity.ok(movieService.getMoviesByDirectorName(directorName));
+    }
+    @GetMapping("/get-all-movies")
+    public ResponseEntity<List<String>> findAllMovies(){
+        return ResponseEntity.ok(movieService.findAllMovies());
+    }
+    @DeleteMapping("/delete-director-by-name")
+    public ResponseEntity<String> deleteDirectorByName(@RequestParam("name") String directorName){
+        return ResponseEntity.ok(movieService.deleteDirectorByName(directorName));
+    }
+    @DeleteMapping("/delete-all-directors")
+    public ResponseEntity<String> deleteAllDirectors(){
+        return ResponseEntity.ok(movieService.deleteAllDirectors());
+    }
+}
---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,66 @@
+package com.driver;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MovieRepository {
+
+    Map<String,Movie> db1=new HashMap<>();
+    Map<String,Director> db2=new HashMap<>();
+
+    Map<String,String> db3=new HashMap<>();
+    public String addMovie(Movie movie) {
+        db1.put(movie.getName(),movie);
+        return "You Have successfully Added your movie...";
+    }
+
+    public String addDirector(Director director) {
+        db2.put(director.getName(),director);
+        return "Successfully Added Director to your database";
+    }
+
+    public String addMovieDirectorPair(String movieName, String directorName) {
+        db3.put(directorName,movieName);
+        return "Successfully Added MovieDirectorPair";
+    }
+
+    public Movie getMovieByName(String movieName) {
+        return db1.get(movieName);
+    }
+
+    public Director getDirectorByName(String directorName) {
+        return db2.get(directorName);
+    }
+
+    public List<String> getMoviesByDirectorName(String directorName) {
+        List<String> list=new ArrayList<>();
+        list.add(db3.get(directorName));
+        return list;
+
+
+    }
+    public List<String> findAllMovies() {
+        List<String> ans=new ArrayList<>();
+        for (Map.Entry<String, Movie> entry : db1.entrySet()) {
+            String key = entry.getKey();
+            ans.add(key);
+        }
+        return ans;
+    }
+
+    public String deleteDirectorByName(String directorName) {
+        db3.remove(directorName);
+        return "Successfully deleted movie and director";
+    }
+
+    public String deleteAllDirectors() {
+        db3.clear();
+        return "Successfully deleted all movies which made by directors";
+    }
+}
